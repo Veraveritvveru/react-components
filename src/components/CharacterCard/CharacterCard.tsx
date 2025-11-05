56bbb3abe46efa3d9d65eae364e991bc6f56956c
Vera Krumpan <filimonovavera96@gmail.com>
2025-11-05 11:12:54 +0300

    refactor: characterCard component from class to function

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React from 'react';
 import type { Character } from '../types';
 import styles from './CharacterCard.module.scss';
 
@@ -6,34 +6,34 @@ interface CharacterCardProps {
   character: Character;
 }
 
-export default class CharacterCard extends Component<CharacterCardProps> {
-  public override render() {
-    return (
-      <div className={styles.card}>
-        <img
-          src={this.props.character.image}
-          alt={this.props.character.name}
-          className={styles.cardImg}
-        />
-        <div className={styles.cardInfo}>
-          <h3 className={styles.cardName}>{this.props.character.name}</h3>
-          <div className={styles.cardInfoItem}>
-            <span>Gender</span>
-            <span>{this.props.character.gender}</span>
-          </div>
-          <div className={styles.cardInfoItem}>
-            <span>Species</span>
-            <span>{this.props.character.species}</span>
-          </div>
-          <div className={styles.cardInfoItem}>
-            <span>Status</span>
-            <span>{this.props.character.status}</span>
-          </div>
-          <div className={styles.cardEpisode}>
-            Appears in {this.props.character.episode.length} episodes
-          </div>
+const CharacterCard: React.FC<CharacterCardProps> = ({ character }) => {
+  return (
+    <div className={styles.card}>
+      <img
+        src={character.image}
+        alt={character.name}
+        className={styles.cardImg}
+      />
+      <div className={styles.cardInfo}>
+        <h3 className={styles.cardName}>{character.name}</h3>
+        <div className={styles.cardInfoItem}>
+          <span>Gender</span>
+          <span>{character.gender}</span>
+        </div>
+        <div className={styles.cardInfoItem}>
+          <span>Species</span>
+          <span>{character.species}</span>
+        </div>
+        <div className={styles.cardInfoItem}>
+          <span>Status</span>
+          <span>{status}</span>
+        </div>
+        <div className={styles.cardEpisode}>
+          Appears in {character.episode.length} episodes
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default CharacterCard;
